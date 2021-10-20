851e97962a4d9ed40b9a710e5352fb3e7efe808c
e-oran <omar.amin@gft.com>
2021-10-20 13:23:23 +0100

    spaces

--- BucketDeploy.sh
+++ BucketDeploy.sh
@@ -5,10 +5,10 @@ gcloud config set project terraform-training-318910
 gcloud config set compute/zone europe-west1-b
 gcloud services enable compute.googleapis.com cloudresourcemanager.googleapis.com
 
-PROJECT_TF_STATE_BUCKET = "REMOTESTATE-OMAR-CICD"
+PROJECT_TF_STATE_BUCKET="REMOTESTATE-OMAR-CICD"
 
-PROJECT = "$(gcloud config get-value core/project)"
+PROJECT="$(gcloud config get-value core/project)"
 
-ZONE = "$(gcloud config get-value compute/zone)"
+ZONE="$(gcloud config get-value compute/zone)"
 
 gsutil ls -b gs://${PROJECT_TF_STATE_BUCKET} || gsutil mb -p ${PROJECT} -b on gs://${PROJECT_TF_STATE_BUCKET}
