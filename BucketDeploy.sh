634736d64c08c54d6f50e06572e74e909003917f
e-oran <omar.amin@gft.com>
2021-10-20 13:14:05 +0100

    cloud build and bucket script

---
+++ BucketDeploy.sh
@@ -0,0 +1,14 @@
+set -e 
+
+gcloud config set project terraform-training-318910
+
+gcloud config set compute/zone europe-west1-b
+gcloud services enable compute.googleapis.com cloudresourcemanager.googleapis.com
+
+PROJECT_TF_STATE_BUCKET = "REMOTESTATE-OMAR-CICD"
+
+PROJECT = "$(gcloud config get-value core/project)"
+
+ZONE = "$(gcloud config get-value compute/zone)"
+
+gsutil ls -b gs://${PROJECT_TF_STATE_BUCKET} || gsutil mb -p ${PROJECT} -b on gs://${PROJECT_TF_STATE_BUCKET}
