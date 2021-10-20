a42735ed2436cb2cef314a2699a108f13a7f3429
e-oran <omar.amin@gft.com>
2021-10-20 16:11:13 +0100

    bucket loop

--- build/BucketDeploy.sh
+++ build/BucketDeploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 set -e 
-RANDOM_NUM=${RANDOM}
+'RANDOM_NUM=${RANDOM}'
 
 gcloud config set project terraform-training-318910
 
 gcloud config set compute/zone europe-west1-b
 gcloud services enable compute.googleapis.com cloudresourcemanager.googleapis.com
 
-PROJECT_TF_STATE_BUCKET="remote_omarbucket_cicd_${RANDOM}"
+PROJECT_TF_STATE_BUCKET="remote_omarbucket_cicd_28011999"
 
 PROJECT="$(gcloud config get-value core/project)"
 
