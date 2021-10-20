9d36157569aad1fcfb8956ce77c719e7793838a5
e-oran <omar.amin@gft.com>
2021-10-20 16:42:32 +0100

    bucket loop

--- build/BucketDeploy.sh
+++ build/BucketDeploy.sh
@@ -13,9 +13,7 @@ PROJECT="$(gcloud config get-value core/project)"
 
 ZONE="$(gcloud config get-value compute/zone)"
 
-BUCKET="$(gsutil ls -b gs://${PROJECT_TF_STATE_BUCKET})"
-
-if [ ! -z "$BUCKET"]
+if [ ! -z "$PROJECT_TF_STATE_BUCKET"]
 then
     echo "${PROJECT_TF_STATE_BUCKET} already existis"
 else
