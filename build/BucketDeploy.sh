6af00c564ae0f8b7a8711568b39e6984f277d067
e-oran <omar.amin@gft.com>
2021-10-20 14:50:09 +0100

    TF BUCKET

--- build/BucketDeploy.sh
+++ build/BucketDeploy.sh
@@ -2,20 +2,18 @@
 
 set -e 
 
-PROJECT_ID="omar-cicd-280199"
-
-gcloud config set project $PROJECT_ID
+gcloud config set project terraform-training-318910
 
 gcloud config set compute/zone europe-west1-b
 gcloud services enable compute.googleapis.com cloudresourcemanager.googleapis.com
 
 PROJECT_TF_STATE_BUCKET="remote_omarbucket_cicd_280199"
 
-PROJECT_ID="$(gcloud config get-value core/project)"
+PROJECT="$(gcloud config get-value core/project)"
 
 ZONE="$(gcloud config get-value compute/zone)"
 
 gsutil ls -b gs://${PROJECT_TF_STATE_BUCKET} || gsutil mb -p ${PROJECT} -b on gs://${PROJECT_TF_STATE_BUCKET}
 
-export PROJECT_ID
-echo $PROJECT_ID
+export PROJECT
+echo $PROJECT
