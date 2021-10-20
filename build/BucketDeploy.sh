df32ac154726e22781a3b4338c8068621b6183df
e-oran <omar.amin@gft.com>
2021-10-20 14:33:54 +0100

    export project

--- build/BucketDeploy.sh
+++ build/BucketDeploy.sh
@@ -14,3 +14,5 @@ PROJECT="$(gcloud config get-value core/project)"
 ZONE="$(gcloud config get-value compute/zone)"
 
 gsutil ls -b gs://${PROJECT_TF_STATE_BUCKET} || gsutil mb -p ${PROJECT} -b on gs://${PROJECT_TF_STATE_BUCKET}
+
+export PROJECT
