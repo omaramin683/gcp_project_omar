8368d3a7660fa0bed269c5b3a70027a8d703cf93
e-oran <omar.amin@gft.com>
2021-10-20 15:29:03 +0100

    destroy files

---
+++ destroy/ProjectDestroy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+#sets the current project to the id of the project we create
+CURRENT_PROJ="$(gcloud projects list --filter 'parent.id=398506523923 AND parent.type=folder AND name:omar-cicd-*' --format='value(project_id)')"
+
+echo $CURRENT_PROJ
+
+#if it exisits then the following will delete, if there is nothing to delete the output will follow
+if [ ! -z "$CURRENT_PROJ" ]
+then
+    gcloud projects delete ${CURRENT_PROJ}
+    echo "DELETING $CURRENT_PROJ" 
+    #exit 0
+else
+    echo "NOTHING TO DELETE"
+fi
+
+exit 0
