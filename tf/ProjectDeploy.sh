fa12bb19876f6fb8d00db84c70e4aca5e2e7401d
e-oran <omar.amin@gft.com>
2021-10-21 15:11:36 +0100

    tf direc

--- tf/ProjectDeploy.sh
+++ tf/ProjectDeploy.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 
+cd ./tf/
+
 #create a project and save it to a variable 
 RANDOM_NUM=${RANDOM}
 export NEW_PROJECT="omar-cicd-${RANDOM_NUM}"
 
 #check if a project called omar-cicd-'random number' exisits in padawan dir
-FILTER_PROJ="$(gcloud projects list --filter 'parent.id=342980323094 AND parent.type=folder AND name:omar-cicd-*')" > project.txt
+FILTER_PROJ="$(gcloud projects list --filter 'parent.id=342980323094 AND parent.type=folder AND name:omar-cicd-*')" >> project.txt
 echo "$FILTER_PROJ already exisits"
 
 #if this does not exisit then the following code is to create a project
