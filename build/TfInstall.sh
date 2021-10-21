edd291e5323c18d3a8d0371efea04150c7e6ef43
e-oran <omar.amin@gft.com>
2021-10-21 10:36:07 +0100

    tf install

---
+++ build/TfInstall.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+echo "Installing Terraform------------------"
+
+sudo apt-get update -y && apt-get upgrade -y 
+
+sudo install wget
+
+echo "Unzip Terraform zip file---------------"
+wget https://releases.hashicorp.com/terraform/0.13.1/terraform_0.13.1_linux_amd64.zip
+unzip terraform_0.13.1_linux_amd64.zip
+
+echo "Move Terraform folder to user/bin directory---------------"
+mv terraform /usr/bin/ 
+
+echo "Remove zip file---------------"
+rm -f terraform_1.0.9_linux_amd64.zip
+
+
+
+VERSION_INSTALLED="$(terraform -version)"
+echo "Version installed: ${VERSION_INSTALLED}"
