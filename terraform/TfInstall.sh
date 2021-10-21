b3232afd90caa3aa5928c080ba7edea761395570
e-oran <omar.amin@gft.com>
2021-10-21 12:50:03 +0100

    tf direc

--- terraform/TfInstall.sh
+++ terraform/TfInstall.sh
@@ -11,13 +11,13 @@ echo "Getting Terraform package from Hashicorp"
 wget https://releases.hashicorp.com/terraform/0.13.1/terraform_0.13.1_linux_amd64.zip
 
 echo "Unzipping Terraform package"
-unzip -f terraform_0.13.1_linux_amd64.zip
+unzip terraform_0.13.1_linux_amd64.zip
 
 echo "Move Terraform folder to user/bin directory"
 mv terraform /usr/bin/ 
 
 echo "Remove zip file"
-#rm -f terraform_1.0.9_linux_amd64.zip
+rm -f terraform_1.0.9_linux_amd64.zip
 
 VERSION_INSTALLED="$(terraform -version)"
 echo "Version installed: ${VERSION_INSTALLED}"
