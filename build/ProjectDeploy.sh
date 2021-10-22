644aba91f937d4e3ecac08f21de176cfd39468ee
e-oran <omar.amin@gft.com>
2021-10-22 11:27:09 +0100

    changes

--- build/ProjectDeploy.sh
+++ build/ProjectDeploy.sh
@@ -6,12 +6,12 @@ RANDOM_NUM=${RANDOM}
 export NEW_PROJECT="omar-cicd-${RANDOM_NUM}"
 
 #check if a project called omar-cicd-'random number' exisits in padawan dir
-FILTER_PROJ="$(gcloud projects list --filter 'parent.id=342980323094 AND parent.type=folder AND name:omar-cicd-*')"
+FILTER_PROJ="$(gcloud projects list --filter 'parent.id=263138525915 AND parent.type=folder AND name:omar-cicd-*')"
 echo "$FILTER_PROJ already exisits"
 
 #if this does not exisit then the following code is to create a project
 if [ -z "$FILTER_PROJ" ]; then
-    gcloud projects create ${NEW_PROJECT} --folder=342980323094
+    gcloud projects create ${NEW_PROJECT} --folder=263138525915
     gcloud config set project ${NEW_PROJECT}
     gcloud config set compute/zone europe-west1-b
     gcloud services enable cloudbilling.googleapis.com
--- destroy/ProjectDestroy.sh
+++ destroy/ProjectDestroy.sh
@@ -2,8 +2,8 @@
 set -e
 
 #sets the current project to the id of the project we create
-CURRENT_PROJ="$(gcloud projects list --filter 'parent.id=342980323094 AND parent.type=folder AND name:omar-cicd-*' --format='value(project_id)')"
-
+CURRENT_PROJ="$(gcloud projects list --filter 'parent.id=263138525915 AND parent.type=folder AND name:omar-cicd-*' --format='value(project_id)')"
+gcloud projects delete ${CURRENT_PROJ}
 echo $CURRENT_PROJ
 
 #if it exisits then the following will delete, if there is nothing to delete the output will follow
