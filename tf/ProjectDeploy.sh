59737758e9fb966bf490b89d97061f12be53d24d
e-oran <omar.amin@gft.com>
2021-10-22 11:13:34 +0100

    tf direc

--- tf/ProjectDeploy.sh
+++ tf/ProjectDeploy.sh
@@ -12,18 +12,20 @@ FILTER_PROJ="$(gcloud projects list --filter 'parent.id=263138525915 AND parent.
 export FILTER_PROJ
 
 echo "$FILTER_PROJ already exisits"
-terraform init
-terraform plan -out activator-plan -var='project=$FILTER_PROJ' #-var-file=./tf/variables.tf
-terraform apply --auto-approve activator-plan
+
 
 #if this does not exisit then the following code is to create a project
 if [ -z "$FILTER_PROJ" ]; then
+    cd ./tf/
     gcloud projects create ${NEW_PROJECT} --folder=263138525915
     gcloud config set project ${NEW_PROJECT}
     gcloud config set compute/zone europe-west1-b
     gcloud services enable cloudbilling.googleapis.com
     gcloud alpha billing projects link ${NEW_PROJECT} --billing-account 01A2F5-73127B-50AE5B
     gcloud services enable compute.googleapis.com cloudresourcemanager.googleapis.com
+    terraform init
+    terraform plan -out activator-plan -var="project=$FILTER_PROJ" #-var-file=./tf/variables.tf
+    terraform apply --auto-approve activator-plan
     exit 0
 [ else gcloud config set project ${FILTER_PROJ}; ]
 export FILTER_PROJ
@@ -33,6 +35,3 @@ echo "$FILTER_PROJ was created"
 fi
 
 exit 0
-sh "terraform init"
-sh "terraform plan -out activator-plan -var='project=$FILTER_PROJ' -var-file=./tf/variables.tf"
-sh "terraform apply --auto-approve activator-plan"
