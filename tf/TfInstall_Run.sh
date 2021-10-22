20bce74639b0554e1654c0db702fe462e04ba401
e-oran <omar.amin@gft.com>
2021-10-22 11:20:59 +0100

    changes

--- tf/TfInstall_Run.sh
+++ tf/TfInstall_Run.sh
@@ -30,11 +30,11 @@ echo "Remove zip file"
 VERSION_INSTALLED="$(terraform -version)"
 echo "Version installed: ${VERSION_INSTALLED}"
 
-echo "tf init---------"
-terraform init 
-echo "tf fmt---------"
-terraform fmt
-echo "tf plan---------"
-terraform plan
-echo "tf apply---------"
-terraform apply --auto-approve
+#echo "tf init---------"
+#terraform init 
+#echo "tf fmt---------"
+#terraform fmt
+#echo "tf plan---------"
+#terraform plan
+#echo "tf apply---------"
+#terraform apply --auto-approve
