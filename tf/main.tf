37e55185cd1fd36a837f6375eea9c5c0e33c2c92
e-oran <omar.amin@gft.com>
2021-10-22 13:10:15 +0100

    changes

--- tf/main.tf
+++ tf/main.tf
@@ -2,6 +2,11 @@ terraform {
   required_providers {
     google = {}
   }
+
+  backend "gcs" {
+    bucket = "UPDATE_ME_BUCKET_OMAR"      #change this  # GCS bucket name to store terraform tfstate
+    prefix = "Omar"           # Update to desired prefix name. Prefix name should be unique for each Terraform project having same remote state bucket.
+  }
 }
 provider "google" {
     version = "3.5.0"
@@ -13,4 +18,6 @@ provider "google" {
 resource "google_storage_bucket" "tf_bucket" {
   name          = "omar_tf_bucket"
   location  = "US"
+  force_destroy = true
+  uniform_bucket_level_access = true
 }
