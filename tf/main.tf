375fdcf6807e5db354a76cd0fe886b30d31eb73e
e-oran <omar.amin@gft.com>
2021-10-22 13:55:47 +0100

    changes

--- tf/main.tf
+++ tf/main.tf
@@ -1,19 +1,6 @@
-terraform {
-  required_providers {
-    google = {}
-  }
-
-  
-}
-provider "google" {
-    version = "3.5.0"
-    region = "us-central1"
-    zone = "us-central1-c"
-    project = "${var.project}"
-}
-
-resource "google_storage_bucket" "tf_bucket" {
-  name          = "omar_tf_bucket"
-  location  = "US"
-  force_destroy = true
+resource "google_storage_bucket" "default" {
+  name = var.bucket_name
+  project = var.project
+  storage_class = var.storage_class
+  location = var.bucket_location
 }
---
+++ tf/terraform.tfvars
@@ -0,0 +1,2 @@
+bucket_name = "omar_tf_gcp_bucket_280199"
+storage_class = "REGIONAL"
