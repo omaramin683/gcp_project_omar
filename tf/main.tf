30f013def8a894188b1a22cde758631b4ea6f9f5
e-oran <omar.amin@gft.com>
2021-10-22 14:02:23 +0100

    changes

--- tf/main.tf
+++ tf/main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "default" {
-  name = var.bucket_name
-  project = var.project
-  storage_class = var.storage_class
-  location = var.bucket_location
+  name = "${var.bucket_name}"
+  project = "${var.project}"
+  storage_class = "${var.storage_class}"
+  location = "${var.bucket_location}"
 }
