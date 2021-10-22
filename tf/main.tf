558d3cf3c24d84f523bfa852973e2d7fbdaf35a8
e-oran <omar.amin@gft.com>
2021-10-22 09:49:10 +0100

    tf direc

--- tf/main.tf
+++ tf/main.tf
@@ -2,12 +2,15 @@ provider "google" {
 version = "3.5.0"
 region = "us-central1"
 zone = "us-central1-c"
+project = "${var.project}"
 }
 
 resource "google_compute_network" "vpc_network" {
+ project = "${var.project}"
 name = "terraform-network"
 }
 resource "google_compute_instance" "vm_instance" {
+project = "${var.project}"
 name = "terraform-instance2"
 machine_type = "f1-micro"
 zone = "us-central1-c"
